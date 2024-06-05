317c0d773b64d72a3d445e8722eb8f0d6d5e106f
bruce007lee <bruce007lee@example.com>
2024-06-05 10:11:05 +0000

    feat: add crop util

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from .face import FaceAlign, FaceAlignImageProcess, FaceAlignMaskProcess
-from .misc import ImageFillColorByMask
+from .misc import ImageFillColorByMask, CropImageByMask
 
 NODE_CLASS_MAPPINGS = {
     "FaceAlign": FaceAlign,
     "FaceAlignImageProcess": FaceAlignImageProcess,
     "FaceAlignMaskProcess": FaceAlignMaskProcess,
     "ImageFillColorByMask": ImageFillColorByMask,
+    "CropImageByMask": CropImageByMask,
 }
