7859b12070d7a6a2bd9f20a057372214b8ee5c4c
lizhen <lizhen@ideal-link.cn>
2024-08-31 21:34:53 +0800

    feat: fix args

--- image.py
+++ image.py
@@ -134,7 +134,7 @@ class LoadImageAdvance:
         return (output_image, output_mask)
 
     @classmethod
-    def IS_CHANGED(s, image):
+    def IS_CHANGED(s, image, mode):
         image_path = folder_paths.get_annotated_filepath(image)
         m = hashlib.sha256()
         with open(image_path, "rb") as f:
@@ -142,7 +142,7 @@ class LoadImageAdvance:
         return m.digest().hex()
 
     @classmethod
-    def VALIDATE_INPUTS(s, image):
+    def VALIDATE_INPUTS(s, image, mode):
         if not folder_paths.exists_annotated_filepath(image):
             return "Invalid image file: {}".format(image)
 
